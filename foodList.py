910f7e716420ed15945e9afed02393573088f7ef
chankyu oh <chankyuoh@u.northwestern.edu>
2017-02-26 17:08:02 -0600

    add ingredients dictionary, cooking tools list

---
+++ foodList.py
@@ -0,0 +1,10 @@
+healthyIngredientsMap = {'sugar': 'stevia', 'salt': 'sea-salt', 'whole milk': 'reduced fat milk', 'butter': 'olive oil',
+              'mayo': 'sour cream', 'mayonnaise': 'sour cream', 'bacon': 'turkey bacon',
+              'all-purpose flour': 'whole wheat flour','ground beef': 'extra lean ground beef'
+              , 'dressing': 'vinegar', 'palm oil': 'olive oil','coconut oil': 'olive oil',
+              'vegetable oil': 'olive oil', 'bread crumbs':'ground flaxseeds','white rice':'brown rice',
+              'pasta': 'whole wheat pasta',}
+
+
+cookingTools = ['pan','grater','whisk','tong','spatula','knife','oven','microwave','skillet','saucepan','pot','bowl','plate'
+                ,'baking sheet','stovetop']
