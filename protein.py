45c181face442fdee27579cbc5a2822dd4da3d6a
tpierce14 <tpierce14@optonline.net>
2017-02-26 17:04:08 -0600

    added tvp

--- protein.py
+++ protein.py
@@ -4,4 +4,4 @@ meat_to_veg = {'chicken' : 'tofu', 'fish' : 'tempeh', 'beef' : 'tofu', 'pork' : 
 			   'salmon' : 'tofu', 'ground' : 'lentils', 'crab' : 'tempeh', 'hamburger' : 'mushroom', 'hot dog' : 'vegetarian hot dog'}
 
 veg_to_meat = {'tofu' : 'chicken', 'tempeh' : 'fish', 'seitan' : 'beef', 'jackfruit' : 'pork', 'mushroom' : 'beef', 'lentils' : 'ground beef',
-				'bean' : 'ground beef', 'legume' : 'beef'}
+				'bean' : 'ground beef', 'legume' : 'beef', 'tvp' : 'ground beef'}
