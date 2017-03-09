ceb5749b2123645cdc5b2674a81ad402d7bc81e0
tpierce14 <tpierce14@optonline.net>
2017-03-09 16:51:20 -0600

    fixed a couple entries

--- mexican-southern.py
+++ mexican-southern.py
@@ -1,6 +1,6 @@
-mexicanToSouthern = { 'Avocado' : 'Sweet Potato', 'Black Beans' : 'Black Eyed Peas', 'Pinto Beans' : 'Black Eyed Peas','Salsa' : 'Gravy', 
+mexicanToSouthern = { 'Avocado' : 'Sweet Potato', 'Beans' : 'Black Eyed Peas', 'Salsa' : 'Gravy', 
 					'Hot Sauce' : 'Barbecue Sauce', 'Chili Pepper' : 'Cayenne Pepper','Maize' : 'Corn', 'Tortilla': 'Cornbread',
 					'Rice': 'Grits', 'Chicken' : 'Pork', 'Lettuce' : 'Collard Greens', 'Chorizo' : 'Sausage',
 					'Tequila' : 'Bourbon', 'Crema' : 'Sour Cream', 'Tomatillo' : 'Tomato', 'Nopales' : 'Okra',
-					'Poblanos' : 'Hot Peppers', 'Chipotle' : 'Cayenne Pepper', 'Steak' : 'Brisket', 'Carne Asada' : 'Chicken Fried Steak',
+					'Poblanos' : 'Cayenne Peppers', 'Chipotle' : 'Cayenne Pepper', 'Steak' : 'Brisket', 'Carne Asada' : 'Chicken Fried Steak',
 					'Guacamole' : 'Gravy', 'Shrimp' : 'Crawfish', 'Crab' : 'Crawfish', 'Squash' : 'Sweet Potato'}
