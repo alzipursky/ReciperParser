8a93ceaf6b221238e0d08e405808256ef6cb11c9
Aaron Zipursky <aaronzipursky@gmail.com>
2017-03-13 09:34:32 -0500

    updated README

---
+++ README.md
@@ -0,0 +1,3 @@
+usage: python main.py [url from allrecipes.com in single quotes] [transformation]
+
+transformations to choose from: vegetarian, meatify, healthy, unhealthy, chinese_to_italian, italian_to_chinese, american_to_middle_eastern, middle_eastern_to_american, mexican_to_soul_food, soul_food_to_mexican, french_to_indian, indian_to_french
--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def find_sub_list(sl, l):
 
 if len(sys.argv) != 3:
     print "usage: python main.py [url of recipe in single quotes] [transformation]"
+    print "transformations to choose from: vegetarian, meatify, healthy, unhealthy, " \
+          "chinese_to_italian, italian_to_chinese, american_to_middle_eastern, middle_eastern_to_american, " \
+          "mexican_to_soul_food, soul_food_to_mexican, french_to_indian, indian_to_french"
     quit()
 
 args = sys.argv
